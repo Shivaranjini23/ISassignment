29f7a9a4f5ea8c69ae182ab92e0058f2e4082426
shivaranjini23 <ranjini3400@gmail.com>
2023-12-23 14:26:47 +0800

    login

--- index.js
+++ index.js
@@ -1,102 +1,41 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+// index.js
 
-app.use(express.json())
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
+const app = express();
+const port = 3000;
+
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Dummy user data (replace with a proper authentication system)
+const users = [
+  { username: 'user1', password: 'password1' },
+  { username: 'user2', password: 'password2' },
+];
+
+// Routes
 app.get('/', (req, res) => {
-   res.send('Hello World!')
-})
+  res.render('login');
+});
 
-/**login admin function*/
-async function login(reqUsername, reqPassword) {
-   return adminCollection.findOne({ username: reqUsername, password: reqPassword })
-     .then(matchUsers => {
-       if (!matchUsers) {
-         return {
-           success: false,
-           message: "Admin not found!"
-         };
-       } else {
-         return {
-           success: true,
-           users: matchUsers
-         };
-       }
-     })
-     .catch(error => {
-         console.error('Error in login:', error);
-         return {
-           success: false,
-           message: "An error occurred during login."
-         };
-       });
- }
- 
- /**create admin function */
- async function register(reqUsername, reqPassword) {
-   return adminCollection.insertOne({
-     username: reqUsername,
-     password: reqPassword,
-     
-   })
-     .then(() => {
-       return "Registration successful!";
-     })
-     .catch(error => {
-       console.error('Registration failed:', error);
-       return "Error encountered!";
-     });
- }
- 
- function generateToken(userData) {
-   const token = jwt.sign(userData, 'inipassword');
-   return token
- 
- }
- 
- function verifyToken(req, res, next) {
-   let header = req.headers.authorization;
-   console.log(header);
- 
-   let token = header.split(' ')[1];
- 
-   jwt.verify(token, 'inipassword', function (err, decoded) {
-     if (err) {
-       res.send('Invalid Token');
-     }
- 
-     req.user = decoded;
-     next();
-   });
- }
- 
- 
 app.post('/login', (req, res) => {
-   console.log(req.body);
- 
-   let result = login(req.body.username, req.body.password);
-   result.then(response => {
-     console.log(response); // Log the response received
- 
-     if (response.success) {
-       let token = generateToken(response.users);
-       res.send(token);
-     } else {
-       res.status(401).send(response.message);
-     }
-   }).catch(error => {
-     console.error('Error in login route:', error);
-     res.status(500).send("An error occurred during login.");
-   });
- });
+  const { username, password } = req.body;
 
+  // Dummy authentication (replace with a proper authentication system)
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if (user) {
+    res.send(`Welcome, ${username}!`);
+  } else {
+    res.send('Invalid username or password.');
+  }
+});
+
+// Start the server
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
- });
-
+  console.log(`Server is running on http://localhost:${port}`);
+});
